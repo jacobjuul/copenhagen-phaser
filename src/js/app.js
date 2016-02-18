17c3dee9c6abb9b6aa4b5ea3e1c7d9dd1bd4f60c
jacobjuul <jacob.juul@gmail.com>
2016-02-18 17:16:40 +0100

    animation

--- src/js/app.js
+++ src/js/app.js
@@ -13,23 +13,29 @@ window.addEventListener('mousewheel', (e) => e.preventDefault());
 
 
 let car, train, bike, taxi, background, cursors, scale;
+let scrolling = { down: false, up: false };
+let direction = { down: false, up: false };
+
 const BG_HEIGHT = 11994,
       BG_WIDTH  = 2560;
 
 const CAR_TURN = {
-  first: { y: 1368, xTo: 1474, yTo: 1779 }
+  first:  { y: 1368, xTo: 1474, yTo: 1779 },
+  second: { y: 4464, xTo: 751,  yTo: 4891 },
+  third:  { y: 7437, xTo: 960,  yTo: 7692 },
+  fourth: { y: 7692, xTo: 1380, yTo: 7692 },
 }
 
 const START_POS = {
-  car: { x: 900, y: 800 },
-  taxi: {x: 1000, y: 100},
-  train: {x: 800, y: 100},
-  bike: {x: 1200, y: 100},
+  car:    { x: 900,   y: 800 },
+  taxi:   { x: 1000,  y: 100 },
+  train:  { x: 800,   y: 100 },
+  bike:   { x: 1200,  y: 100 },
 
 };
 
 let speed = {
-  car: 600,
+  car: 1000,
   bike: 600,
 }
 
@@ -54,69 +60,56 @@ function create() {
   game.world.setBounds(0, 0, BG_WIDTH * scale, BG_HEIGHT * scale);
 
   // Add the sprites
-  car = game.add.sprite(START_POS.car.x * scale, START_POS.car.y * scale, 'car');
+  car   = game.add.sprite(START_POS.car.x * scale, START_POS.car.y * scale, 'car');
   train = game.add.sprite(START_POS.train.x * scale, START_POS.train.y * scale, 'train');
-  bike = game.add.sprite(START_POS.bike.x * scale, START_POS.bike.y * scale, 'bike');
-  taxi = game.add.sprite(START_POS.taxi.x * scale, START_POS.taxi.y * scale, 'taxi');
+  bike  = game.add.sprite(START_POS.bike.x * scale, START_POS.bike.y * scale, 'bike');
+  taxi  = game.add.sprite(START_POS.taxi.x * scale, START_POS.taxi.y * scale, 'taxi');
 
   let spritesArr = [car, train, bike, taxi, background];
 
   for (let i in spritesArr) {
     spritesArr[i].scale.setTo(scale);
     if (i < 4) {
-      spritesArr[i].anchor.setTo(0.5, 0.5);
+      spritesArr[i].anchor.set(0.5);
       game.physics.arcade.enable(spritesArr[i], Phaser.Physics.ARCADE);
 
-      spritesArr[i].body.maxVelocity.set(800);
+      spritesArr[i].body.maxVelocity.set(400, 400);
 
     }
   }
 
 
-
   cursors = game.input.keyboard.createCursorKeys();
-
   game.camera.follow(car);
+  car.body.drag.set(2500 * scale);
+  car.body.maxVelocity.set(400);
 
-
-  game.input.mouse.mouseWheelCallback = mouseWheel;
-
+  game.input.mouse.mouseWheelCallback = mouseWheel; 
 
 }
 
 
 function mouseWheel(event) {
   switch (game.input.mouse.wheelDelta) {
+
     case 1:
-      car.body.velocity.y = -speed.car;
-      game.physics.arcade.accelerateToXY(car, 0,0, 0, 0,0);
+      scrolling.up = true;
+      scrolling.down = false;
+
       break;
+
+
     case -1:
-
-
-     if (car.position.y >= CAR_TURN.first.y * scale && car.position.y < CAR_TURN.first.yTo * scale) {
-       car.body.velocity.y = 0;
-     } else {
-       car.body.velocity.y = speed.car * scale;
-     }
+      scrolling.up = false;
+      scrolling.down = true;
 
     default:
+
 
   };
 
 }
 
-function movePoint() {
-  if (car.position.y >= CAR_TURN.first.y * scale) {
-   let rotation = game.physics.arcade.accelerateToXY(car, CAR_TURN.first.xTo * scale, CAR_TURN.first.yTo * scale, 20000 * scale, 20000, 20000);
-   car.rotation = rotation * -1;
-  }
-
-  if (car.position.x >= CAR_TURN.first.xTo * scale) {
-    game.physics.arcade.accelerateToXY(car, 0,0,0,0);
-    car.rotation = 0;
-  }
-}
 
 
 function update() {
@@ -124,35 +117,84 @@ function update() {
   game.physics.arcade.collide(car, bike);
   game.physics.arcade.collide(car, taxi);
 
-  console.log(car.position.x)
+  // Conditionals
+  let carWithinFirst = car.position.y >= CAR_TURN.first.y * scale && car.position.y <= CAR_TURN.first.yTo * scale;
+  let carWithinSecond = car.position.y >= CAR_TURN.second.y * scale && car.position.y <= CAR_TURN.second.yTo * scale;
+  let carWithinThird = car.position.y >= CAR_TURN.third.y * scale && car.position.y <= CAR_TURN.third.yTo * scale;
+  let carWithinFourth = false;
 
-  movePoint();
+  if (car.position.y > CAR_TURN.first.y * scale && car.position.y < CAR_TURN.first.yTo * scale) {
+    scrolling.down = true;
+  } 
 
+  if (car.position.y > CAR_TURN.second.y * scale && car.position.y < CAR_TURN.second.yTo * scale) {
+    scrolling.down = true;
+  }  
 
-  car.body.velocity.x = 0;
-  car.body.velocity.y = 0;
+  if (car.position.y > CAR_TURN.third.y * scale && car.position.y < CAR_TURN.third.yTo * scale) {
+    scrolling.down = true;
+  }  
 
-  if (cursors.left.isDown)
-  {
-    //  Move to the left
-    car.body.velocity.x = -450;
+  if (car.position.y >= CAR_TURN.fourth.y * scale && car.position.y <= CAR_TURN.fourth.yTo * scale) {
+    scrolling.down = true;
+  } 
+  
 
+  if (scrolling.down) {
+   
+   // FIRST MOVEMENT POINT
+    if (carWithinFirst) {
+      let rotation = game.physics.arcade.angleToXY(car, CAR_TURN.first.xTo * scale, CAR_TURN.first.yTo * scale);
+      console.log(rotation)
+      car.rotation = -rotation - 0.5;
+      game.physics.arcade.velocityFromRotation(rotation, speed.car * scale, car.body.velocity);
+
+    } else {
+      car.body.velocity.y = speed.car * scale;
+      car.rotation = 0;
+    }
+
+    // Second movement Point
+    if (carWithinSecond) {
+      let rotation = game.physics.arcade.angleToXY(car, CAR_TURN.second.xTo * scale, CAR_TURN.second.yTo * scale);
+      console.log(rotation);
+
+      car.rotation = (-rotation * -rotation + 0.5);
+      game.physics.arcade.velocityFromRotation(rotation, speed.car * scale, car.body.velocity);
+    }
+
+    // Third movement Point
+    if (carWithinThird) {
+      let rotation = game.physics.arcade.angleToXY(car, CAR_TURN.third.xTo * scale, CAR_TURN.third.yTo * scale);
+      console.log(rotation)
+      car.rotation = -rotation - 0.5;
+      game.physics.arcade.velocityFromRotation(rotation, speed.car * scale, car.body.velocity);
+    }
+
+    // Fourth movement point
+    if (carWithinFourth) {
+      
+      game.physics.arcade.velocityFromRotation(-1.5, speed.car * scale, car.body.velocity);
+    }
+    
+    scrolling.down = false;
+    scrolling.up = false;
 
   }
-  else if (cursors.right.isDown)
-  {
-    //  Move to the right
-    car.body.velocity.x = 450;
 
-
-  }  else if (cursors.up.isDown)
-  {
-    car.body.velocity.y = -450;
-  } else if (cursors.down.isDown) {
-    car.body.velocity.y = 450;
+  if (scrolling.up) {
+    
+    //  Move to the left
+    car.body.velocity.y = -speed.car * scale;
+    scrolling.up = false;
+    scrolling.down = false;
   }
 }
 
+
 function render() {
-  game.debug.spriteCoords(car, 32, 500);
+  // game.debug.spriteCoords(car, 32, 500);
+
+    game.debug.body(car);
+    game.debug.spriteInfo(car, 30, 30);
 }
